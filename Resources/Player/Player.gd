6f4e4fef6c997d5d52b66bb22c1229b6b365667d
Alex Kharouk <akharoukpp@gmail.com>
2020-07-02 18:07:05 +0200

    Set up more keybindings and attack animation

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -4,14 +4,36 @@ const ACCELERATION := 500
 const FRICTION := 500
 const MAX_SPEED := 100
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 var velocity := Vector2.ZERO
 
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
+
+onready var animationPlayer := $AnimationPlayer
+onready var animationTree := $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready() -> void:
+	animationTree.active = true
 
 func _physics_process(delta) -> void:
+	match state:
+		MOVE:
+			move_state(delta)
+			
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta: float) -> void:
 	var input_vector := Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -31,3 +53,9 @@ func _physics_process(delta) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state(delta: float) -> void:
+	pass
