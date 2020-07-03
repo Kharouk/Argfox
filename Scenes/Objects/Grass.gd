8a98f7e8b2b74714128d0903e73947a772620fbc
Alex Kharouk <akharoukpp@gmail.com>
2020-07-03 12:36:04 +0200

    Finish adding grass cut effect globally

--- Scenes/Objects/Grass.gd
+++ Scenes/Objects/Grass.gd
@@ -2,4 +2,11 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://Scenes/Effects/GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance()
+		
+		var world = get_tree().current_scene
+		world.add_child(grassEffect, true)
+		
+		grassEffect.global_position = global_position
 		queue_free()
